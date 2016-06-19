f82fc477e50bbdb29f95502b51c5d8471cae24cb
threezj <threezj@hotmail.com>
2016-06-20 00:27:24 +0800

    finish hw8

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,17 +1,26 @@
 
 (define (deep-map fn s)
-  ; YOUR-CODE-HERE
-  nil
-)
+	(if (null? s) '() 
+		(if (list? s) ( cons (deep-map fn (car s)) (deep-map fn (cdr s))) (fn  s)) 
+))
 
 (define (substitute s old new)
-  ; YOUR-CODE-HERE
-  nil
+ 	(if (null? s) '() 
+		(if (list? s) (cons (substitute (car s) old new) (substitute (cdr s) old new)) (if (equal? s old) new s)) 
+	)
 )
 
 (define (sub-all s olds news)
-  ; YOUR-CODE-HERE
-  nil
+ 	
+	(define (inlist? word news olds) 
+		(if (null? news) word (if (equal? word (car news)) (car olds) (inlist? word (cdr news) (cdr olds))))	
+	)
+
+	(if (null? s) '() 
+		(if (list? s) 
+			(cons (sub-all (car s) olds news) (sub-all (cdr s) olds news)) 
+			(inlist? s olds news)) 
+	)
 )
 
 
